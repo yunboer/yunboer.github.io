3052a524b6c19f3630dec747238aa4ddacdefd5a
yunboer <996456575@qq.com>
2024-11-20 00:13:50 +0800

    modify script for auto build

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
-import withMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  output: 'export' as 'export', // 强制将 output 类型指定为 'export'
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+  output: 'export',
   images: {
     unoptimized: true,
   },
 };
 
-export default withMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-})(nextConfig);
+export default nextConfig;
